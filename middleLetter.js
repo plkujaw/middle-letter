c49eb909e0574fd23c3964cdc01f4887c936d615
plkujaw <kuba.jawor@gmail.com>
2020-09-17 11:23:44 +0100

    return a middle letter if the given word's length is odd

--- middleLetter.js
+++ middleLetter.js
@@ -1,3 +1,7 @@
-const getMiddle = () => {};
+const getMiddle = (word) => {
+  if (word.length % 2 !== 0) {
+    return word.slice(word.length / 2, word.length / 2 + 1);
+  }
+};
 
 module.exports = getMiddle;
--- middleLetter.test.js
+++ middleLetter.test.js
@@ -1,1 +1,5 @@
 import getMiddle from "./middleLetter.js";
+
+test("should return a middle letter if the given word's length is odd", () => {
+  expect(getMiddle("testing")).toBe("t");
+});
