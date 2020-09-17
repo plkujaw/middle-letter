802776299c31e49ab07c8fe5c6325667153f0cd6
plkujaw <kuba.jawor@gmail.com>
2020-09-17 11:29:06 +0100

    return the middle 2 letters if the word's length is even

--- middleLetter.js
+++ middleLetter.js
@@ -1,6 +1,8 @@
 const getMiddle = (word) => {
   if (word.length % 2 !== 0) {
     return word.slice(word.length / 2, word.length / 2 + 1);
+  } else {
+    return word.slice(word.length / 2 - 1, word.length / 2 + 1);
   }
 };
 
--- middleLetter.test.js
+++ middleLetter.test.js
@@ -1,5 +1,11 @@
 import getMiddle from "./middleLetter.js";
 
-test("should return a middle letter if the given word's length is odd", () => {
+test("should return the middle letter if the given word's length is odd", () => {
   expect(getMiddle("testing")).toBe("t");
+  expect(getMiddle("A")).toBe("A");
+});
+test("should return the middle 2 letters if the word's length is even", () => {
+  expect(getMiddle("test")).toBe("es");
+  expect(getMiddle("middle")).toBe("dd");
+  expect(getMiddle("of")).toBe("of");
 });
